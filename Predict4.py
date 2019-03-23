ecd1e97f5fc84572b725b04044224728e25879ae
Jan Kot <jan.kot.star@gmail.com>
2019-03-23 23:01:20 +0100

    Update Predict4.py

--- Predict4.py
+++ Predict4.py
@@ -17,7 +17,6 @@ X = X.drop([26], axis='columns')
 
 y = pd.DataFrame(dataset.iloc[:, 28].values)
 
-
 #Taking care of missing data
 
 from sklearn.preprocessing import Imputer
@@ -29,26 +28,13 @@ imputer2 = Imputer(missing_values='NaN',strategy='mean', axis = 0)
 imputer2 = imputer2.fit(X.iloc[:, 0:1])
 X.iloc[:, 0:1] = imputer.transform(X.iloc[:, 0:1])
 
-
 imputerY = Imputer(missing_values='NaN',strategy='mean', axis = 0)
 imputerY = imputerY.fit(y.iloc[:, 0:1])
 y.iloc[:, 0:1] = imputer.transform(y.iloc[:, 0:1])
 
-#Encoding categorical data
-'''
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X = LabelEncoder()
-X.iloc[:, 0] = labelencoder_X.fit_transform(X.iloc[:, 0])
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
-
-labelencoder_y = LabelEncoder() 
-y = labelencoder_X.fit_transform(y)
-'''
 
 from keras.utils.np_utils import to_categorical
 y_binary = to_categorical(y)
-
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -60,14 +46,7 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Tips:
-  
-
-
-
-
 # PREDICTING
-
 
 import keras
 from keras.models import Sequential
